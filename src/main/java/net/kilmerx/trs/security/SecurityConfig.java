2967077191167ac69e266e3f26de348692717ae2
ouissoune ilyas <ilyasyassine41@gmail.com>
2026-01-22 12:17:12 +0100

    Change frontend URL to use HTTPS in CORS config
    Updated CORS configuration to use HTTPS for the frontend URL.

--- src/main/java/net/kilmerx/trs/security/SecurityConfig.java
+++ src/main/java/net/kilmerx/trs/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "trs-frontend-n7na2u6mz-ilyasyassine41-gmailcoms-projects.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://trs-frontend-n7na2u6mz-ilyasyassine41-gmailcoms-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
